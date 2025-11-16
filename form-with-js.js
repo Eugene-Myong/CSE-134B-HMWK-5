d2049cfe4dbfbd35e150dee69f0ad1e2f80ca048
Eugene-Myong <esmyong@ucsd.edu>
2025-11-16 13:58:45 -0800

    Update form-with-js
    Added theme toggler to form-with-js

--- form-with-js.js
+++ form-with-js.js
@@ -1,3 +1,21 @@
+const toggleBtn = document.getElementById("toggle-btn");
+    const body = document.body;
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+    toggleBtn.addEventListener("click", () => {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+            localStorage.setItem("theme", "dark");
+        } else {
+            localStorage.setItem("theme", "light");
+        }
+    });
+
 const form_errors = [];
 
 function loadHandler() {
